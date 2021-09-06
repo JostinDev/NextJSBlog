609e29a032c4ad74e60484eb6a6a140fa44c0245
JostinDev <justinnydegger@hotmail.com>
2021-09-06 21:08:49 +0200

    Add project card

--- component/post.js
+++ component/post.js
@@ -17,11 +17,8 @@ export default function Post({title, id, date, img, isFirstPost}) {
       <div className='flex flex-col flex-wrap h-full w-full md:w-1/2 lg:w-1/3 inline-flex px-4 mb-10'>
         <Image src={img} height={1080} width={1920} layout={"intrinsic"} alt={''} className='max-w rounded'/>
         <p className='mt-2 text-gray-500 text-sm'>{monthNames[month-1] + ' ' + day + ', ' + year}</p>
-        <p className='text-gray-700 text-lg font-medium'>{title}</p>
         <Link href={'posts/' + id} passHref>
-          <a type="button" className="w-max mt-2 py-2 px-4 font-semibold cursor-pointer rounded text-white bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400">
-            Read
-          </a>
+          <p className='inline-block cursor-pointer text-lg font-medium text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400'>{title}</p>
         </Link>
       </div>
     )
@@ -33,11 +30,8 @@ export default function Post({title, id, date, img, isFirstPost}) {
         </div>
         <div className='flex flex-col md:pl-6'>
           <p className='mt-2 mb-3 text-gray-500 text-sm'>{monthNames[month-1] + ' ' + day + ', ' + year}</p>
-          <p className='text-lg md:text-3xl lg:text-4xl font-medium md:font-normal'>{title}</p>
           <Link href={'posts/' + id} passHref>
-            <a type="button" className="w-max mt-3 py-3 px-6 font-semibold cursor-pointer rounded text-white bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400">
-              Read
-            </a>
+            <p className='inline-block text-lg cursor-pointer md:text-3xl lg:text-4xl font-medium md:font-normal text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400'>{title}</p>
           </Link>
         </div>
       </div>
---
+++ component/project.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Link from "next/link";
+
+export default function Project({title, description, date, img, link}) {
+
+    return (
+      <div className='flex flex-col w-1/3'>
+        <Image src={img} height={1080} width={1920} layout={"intrinsic"} alt={''} className='rounded'/>
+        <p className='mt-2 text-gray-500 text-sm'>{date}</p>
+          <a target="_blank" href={link} rel="noreferrer">
+              <p className='inline-block cursor-pointer text-lg font-semibold py-1 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400'>
+                  {title}
+              </p>
+          </a>
+        <p className='text-gray-800'>{description}</p>
+      </div>
+    )
+}
