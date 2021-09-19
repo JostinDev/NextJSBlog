2e950a188e07e15ab766c4c7fcd1e44f6a24b94f
JostinDev <justinnydegger@hotmail.com>
2021-09-19 19:45:18 +0200

    Update navbar style

--- component/navbar.js
+++ component/navbar.js
@@ -53,14 +53,14 @@ function Navbar () {
   }
 
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='border-b bg-white border-gray-300'>
       {({ open }) => (
         <>
           <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
             <div className='relative flex items-center justify-between h-16'>
               <div className='absolute inset-y-0 right-0 flex items-center sm:hidden'>
                 {/* Mobile menu button */}
-                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-500'>
                   <span className='sr-only'>Open main menu</span>
                   {open
                     ? (<XIcon className='block h-6 w-6' aria-hidden='true' />)
@@ -69,12 +69,7 @@ function Navbar () {
               </div>
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex-shrink-0 flex items-center'>
-                  <img
-                    className='block h-8 w-auto'
-                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
-                    alt='Workflow'
-                  />
-
+                  <p className='block h-8 w-auto text-3xl font-semibold text-transparent cursor-pointer bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'>JN</p>
                 </div>
                 <div className='hidden sm:block sm:ml-6'>
                   <div className='flex space-x-4'>
@@ -87,8 +82,10 @@ function Navbar () {
                         <span
                             onClick={() => changeFocus(item)}
                             className={classNames(
-                            item.current ? 'bg-gray-900 text-white cursor-pointer' : 'cursor-pointer text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium'
+                            item.current ?
+                              'py-1 px-1 text-white rounded cursor-pointer bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400'
+                              :
+                              'py-1 text-transparent cursor-pointer bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'
                             )}
                             aria-current={item.current ? 'page' : undefined}>
                           {item.name}
@@ -112,8 +109,10 @@ function Navbar () {
                   <p
                       onClick={() => changeFocus(item)}
                       className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
+                        item.current ?
+                          'py-1 px-1 text-white rounded cursor-pointer bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400'
+                          :
+                          'py-1 text-transparent cursor-pointer bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'
                       )}
                       aria-current={item.current ? 'page' : undefined}
                   >
