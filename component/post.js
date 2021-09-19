1ea189e8d1003ddbc9ffe4941738fae25e376300
JostinDev <justinnydegger@hotmail.com>
2021-09-19 18:23:35 +0200

    Change home page title

--- component/post.js
+++ component/post.js
@@ -15,7 +15,9 @@ export default function Post({title, id, date, img, isFirstPost}) {
   if(!isFirstPost) {
     return (
       <div className='flex flex-col flex-wrap h-full w-full md:w-1/2 lg:w-1/3 inline-flex px-4 mb-10'>
-        <Image src={img} height={1080} width={1920} layout={"intrinsic"} alt={''} className='max-w rounded'/>
+        <Link href={'posts/' + id} passHref>
+          <Image src={img} height={1080} width={1920} layout={"intrinsic"} alt={''} className='max-w rounded cursor-pointer'/>
+        </Link>
         <p className='mt-2 text-gray-500 text-sm'>{monthNames[month-1] + ' ' + day + ', ' + year}</p>
         <Link href={'posts/' + id} passHref>
           <p className='inline-block cursor-pointer text-lg font-medium text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-300 hover:via-pink-400 hover:to-red-400'>{title}</p>
@@ -26,7 +28,9 @@ export default function Post({title, id, date, img, isFirstPost}) {
     return (
       <div className='flex flex-col md:flex-row w-full px-4 mb-10'>
         <div className='w-full md:w-4/6'>
-          <Image src={img} height={1080} width={1920} layout={"intrinsic"} alt={''} className='rounded'/>
+          <Link href={'posts/' + id} passHref>
+            <Image src={img} height={1080} width={1920} layout={"intrinsic"} alt={''} className='rounded cursor-pointer'/>
+          </Link>
         </div>
         <div className='flex flex-col md:pl-6'>
           <p className='mt-2 mb-3 text-gray-500 text-sm'>{monthNames[month-1] + ' ' + day + ', ' + year}</p>
