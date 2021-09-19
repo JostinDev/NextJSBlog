f05b88eef6ce26732ed5db3a74e8e4be4e3b4710
JostinDev <justinnydegger@hotmail.com>
2021-09-19 18:53:34 +0200

    Remove dev dependencies

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import * as THREE from "three";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {useEffect} from "react";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
-import Stats from 'stats.js'
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 
 
